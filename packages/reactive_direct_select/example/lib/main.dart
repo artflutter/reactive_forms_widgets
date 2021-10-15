2a022b2dbcf7803389a213385c64760cf1911b75
vasilich <vasilich@grafin.kiev.ua>
2021-10-15 12:34:01 +0300

    reactive_direct_select 0.0.1

---
+++ packages/reactive_direct_select/example/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:reactive_direct_select/reactive_direct_select.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  FormGroup buildForm() => fb.group({
+        'input': FormControl<String>(value: 'Tunisia'),
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 20.0,
+            ),
+            child: ReactiveFormBuilder(
+              form: buildForm,
+              builder: (context, form, child) {
+                return Column(
+                  children: [
+                    ReactiveDirectSelect<String, String>(
+                      formControlName: 'input',
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        suffixIcon: Icon(Icons.arrow_drop_down),
+                      ),
+                      itemExtent: 35.0,
+                      items: const [
+                        'Tunisia',
+                        'Brazil',
+                      ],
+                      itemBuilder: (context, item) =>
+                          item == null ? const SizedBox.shrink() : Text(item),
+                      selectedItemBuilder: (context, item) =>
+                          item == null ? const SizedBox.shrink() : Text(item),
+                    ),
+                    ElevatedButton(
+                      child: Text('Sign Up'),
+                      onPressed: () {
+                        if (form.valid) {
+                          print(form.value);
+                        } else {
+                          form.markAllAsTouched();
+                        }
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ packages/reactive_direct_select/test/reactive_direct_select_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:reactive_direct_select/reactive_direct_select.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
