3e2edbb013bfcb82d16b793701b0e5d5d0253450
vasilich <vasilich@grafin.kiev.ua>
2021-12-16 15:37:07 +0200

    reactive_text_field 0.0.1

---
+++ packages/reactive_text_field/README.md
@@ -0,0 +1,5 @@
+# reactive_touch_spin
+
+Wrapper around [`TextField`](https://api.flutter.dev/flutter/material/TextField-class.html) to use with [`reactive_forms`](https://pub.dev/packages/reactive_forms)
+
+Docs in progress. See [`example`](https://github.com/artflutter/reactive_forms_widgets/tree/master/packages/reactive_text_field/example) folder for sample.
---
+++ packages/reactive_text_field/example/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:reactive_forms/reactive_forms.dart' hide ReactiveTextField;
+import 'package:reactive_text_field/reactive_text_field.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController? _controller;
+
+  FormGroup buildForm() => fb.group({
+        'input': FormControl<String>(value: 'some value'),
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 20.0,
+            ),
+            child: ReactiveFormBuilder(
+              form: buildForm,
+              builder: (context, form, child) {
+                return Column(
+                  children: [
+                    ReactiveTextField<String>(
+                      formControlName: 'input',
+                      onControllerInit: (controller) {
+                        _controller = controller;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      child: const Text('Sign Up'),
+                      onPressed: () {
+                        if (form.valid) {
+                          // ignore: avoid_print
+                          print(form.value);
+                        } else {
+                          form.markAllAsTouched();
+                        }
+                      },
+                    ),
+                    ElevatedButton(
+                      child: const Text('Select all text'),
+                      onPressed: () {
+                        if (_controller != null) {
+                          form.control('input').focus();
+
+                          _controller?.selection = TextSelection(
+                            baseOffset: 0,
+                            extentOffset: _controller?.text.length ?? 0,
+                          );
+                        }
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
