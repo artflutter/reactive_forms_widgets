c8eee46fe5ded9b452303574350eb1f899ed349d
vasilich <vasilich@grafin.kiev.ua>
2022-01-26 10:29:09 +0200

    reactive_flutter_typeahead 0.2.0

--- packages/reactive_flutter_typeahead/lib/reactive_flutter_typeahead.dart
+++ packages/reactive_flutter_typeahead/lib/reactive_flutter_typeahead.dart
@@ -1,5 +1,5 @@
 library reactive_flutter_typeahead;
 
-export 'package:flutter_typeahead2/flutter_typeahead2.dart';
+export 'package:flutter_typeahead/flutter_typeahead.dart';
 export 'src/reactive_typeahead.dart';
 export 'src/reactive_cupertino_typeahead.dart';
--- packages/reactive_flutter_typeahead/lib/src/reactive_typeahead.dart
+++ packages/reactive_flutter_typeahead/lib/src/reactive_typeahead.dart
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_typeahead2/flutter_typeahead2.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
 /// A [ReactiveTypeAhead] that contains a [TextField].
