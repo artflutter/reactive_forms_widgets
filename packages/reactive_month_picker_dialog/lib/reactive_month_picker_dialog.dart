7ea70470f4c55e05db60c3ed9b63fb19da261173
Vasiliy Ditsyak <vasilich6107@users.noreply.github.com>
2022-09-17 19:57:37 +0200

    reactive_month_picker_dialog 0.5.0

--- packages/reactive_month_picker_dialog/lib/reactive_month_picker_dialog.dart
+++ packages/reactive_month_picker_dialog/lib/reactive_month_picker_dialog.dart
@@ -2,7 +2,7 @@ library reactive_month_picker_dialog;
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:month_picker_dialog/month_picker_dialog.dart';
+import 'package:month_picker_dialog_2/month_picker_dialog_2.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
 final _effectiveValueAccessor = DateTimeValueAccessor(
