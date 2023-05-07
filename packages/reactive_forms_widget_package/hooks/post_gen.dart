39a46714d2b9ff1492b910ac1d47881394461696
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-05-07 20:26:28 +0300

    * Improve hooks/post_gen * Minimize generated package pubspec.yaml * remove mason-lock.json

--- packages/reactive_forms_widget_package/hooks/post_gen.dart
+++ packages/reactive_forms_widget_package/hooks/post_gen.dart
@@ -4,35 +4,75 @@ import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
   final originalPackageName = context.vars['originalPackageName'];
-  final package = 'reactive_${originalPackageName}';
-  final packageExample = '$package/example';
+  final reactivePackageName = 'reactive_${originalPackageName}';
+  final reactivePackageDir = Directory(reactivePackageName);
+  context.logger
+      .info('############### CREATE $reactivePackageName ###############');
+  await _runProcess(
+    'flutter',
+    [
+      'create',
+      reactivePackageName,
+      '--template=package',
+    ],
+    workingDirectory: reactivePackageDir.parent.path,
+  );
 
-  context.logger.info('############### CREATE $package ###############');
-  await Process.run('flutter', [
-    'create',
-    package,
-    '--template=package',
-  ]).then((value) {
-    print(value.stdout);
-    print(value.stderr);
-    print(value.exitCode);
-  });
+  context.logger.info(
+      '############### CREATE $reactivePackageName/example ###############');
+  await _runProcess(
+    'flutter',
+    ['create', 'example', '--template=app', '-e', '--no-pub'],
+    workingDirectory: reactivePackageDir.path,
+  );
 
   context.logger
-      .info('############### CREATE $package/example ###############');
-  await Process.run('flutter', ['create', '.'],
-          workingDirectory: packageExample)
-      .then((value) {
-    print(value.stdout);
-    print(value.stderr);
-    print(value.exitCode);
-  });
+      .info('############### ADD latest dependencies ###############');
+  await _runProcess(
+    'flutter',
+    [
+      'pub',
+      'add',
+      'reactive_forms',
+      'dev:flutter_lints',
+      originalPackageName,
+    ],
+    workingDirectory: reactivePackageDir.path,
+  );
+  await _runProcess(
+    'flutter',
+    [
+      'pub',
+      'add',
+      'reactive_forms',
+      '$reactivePackageName:{"path":"../"}',
+    ],
+    workingDirectory: Directory('${reactivePackageDir.path}/example').path,
+  );
+}
 
-  context.logger.info('############### RUN flutter pub get ###############');
-  await Process.run('flutter', ['pub', 'get'], workingDirectory: package)
-      .then((value) {
-    print(value.stdout);
-    print(value.stderr);
-    print(value.exitCode);
-  });
+// Modified version from https://github.com/flutter/packages/blob/main/packages/go_router/tool/run_tests.dart#L33-L51
+Future<Process> _streamOutput(Future<Process> processFuture) async {
+  final Process process = await processFuture;
+  stdout.addStream(process.stdout);
+  stderr.addStream(process.stderr);
+  return process;
+}
+
+Future<int> _runProcess(
+  String command,
+  List<String> arguments, {
+  String? workingDirectory,
+}) async {
+  final Process process = await _streamOutput(Process.start(
+    command,
+    arguments,
+    workingDirectory: workingDirectory,
+    runInShell: true,
+  ));
+  final exitCode = await process.exitCode;
+  if (exitCode != 0) {
+    exit(exitCode);
+  }
+  return process.exitCode;
 }
