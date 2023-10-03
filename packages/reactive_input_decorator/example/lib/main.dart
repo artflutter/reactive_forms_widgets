fc8ae1be1062f6eee9f41bc75aa5ef14fc291add
Vasiliy Ditsyak <vasilich6107@users.noreply.github.com>
2023-10-03 11:36:07 +0200

    reactive_input_decorator 0.0.1

---
+++ packages/reactive_input_decorator/example/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:reactive_input_decorator/reactive_input_decorator.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  FormGroup buildForm() => fb.group({
+        'input': FormControl<bool>(
+          value: null,
+          validators: [const RequiredValidator()],
+        ),
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 20.0,
+            ),
+            child: ReactiveFormBuilder(
+              form: buildForm,
+              builder: (context, form, child) {
+                return Column(
+                  children: [
+                    ReactiveInputDecorator(
+                      formControlName: 'input',
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        isDense: true,
+                        isCollapsed: true,
+                        helperText: "",
+                        helperStyle: TextStyle(height: 0.8),
+                        errorStyle: TextStyle(height: 0.8),
+                      ),
+                      child: Row(
+                        children: [
+                          const SizedBox(width: 16),
+                          const Expanded(child: Text('Some label')),
+                          ReactiveCheckbox(formControlName: 'input'),
+                        ],
+                      ),
+                    ),
+                    ElevatedButton(
+                      child: const Text('Submit'),
+                      onPressed: () {
+                        if (form.valid) {
+                          debugPrint(form.value.toString());
+                        } else {
+                          form.markAllAsTouched();
+                        }
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ packages/reactive_input_decorator/lib/src/reactive_input_decorator.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+
+/// A [ReactiveInputDecorator] that contains a [InputDecorator].
+///
+/// This is a convenience widget that wraps a [InputDecorator] widget in a
+/// [ReactiveInputDecorator].
+///
+/// A [ReactiveForm] ancestor is required.
+///
+class ReactiveInputDecorator extends ReactiveFormField<dynamic, dynamic> {
+  /// Creates a [ReactiveInputDecorator] that contains a [InputDecorator].
+  ///
+  /// Can optionally provide a [formControl] to bind this widget to a control.
+  ///
+  /// Can optionally provide a [formControlName] to bind this ReactiveFormField
+  /// to a [FormControl].
+  ///
+  /// Must provide one of the arguments [formControl] or a [formControlName],
+  /// but not both at the same time.
+  ///
+  /// Can optionally provide a [validationMessages] argument to customize a
+  /// message for different kinds of validation errors.
+  ///
+  /// Can optionally provide a [valueAccessor] to set a custom value accessors.
+  /// See [ControlValueAccessor].
+  ///
+  /// Can optionally provide a [showErrors] function to customize when to show
+  /// validation messages. Reactive Widgets make validation messages visible
+  /// when the control is INVALID and TOUCHED, this behavior can be customized
+  /// in the [showErrors] function.
+  ///
+  /// ### Example:
+  /// Binds a text field.
+  /// ```
+  /// final form = fb.group({'email': Validators.required});
+  ///
+  /// ReactiveInputDecorator(
+  ///   formControlName: 'email',
+  /// ),
+  ///
+  /// ```
+  ///
+  /// Binds a text field directly with a *FormControl*.
+  /// ```
+  /// final form = fb.group({'email': Validators.required});
+  ///
+  /// ReactiveInputDecorator(
+  ///   formControl: form.control('email'),
+  /// ),
+  ///
+  /// ```
+  ///
+  /// Customize validation messages
+  /// ```dart
+  /// ReactiveInputDecorator(
+  ///   formControlName: 'email',
+  ///   validationMessages: {
+  ///     ValidationMessage.required: 'The email must not be empty',
+  ///     ValidationMessage.email: 'The email must be a valid email',
+  ///   }
+  /// ),
+  /// ```
+  ///
+  /// Customize when to show up validation messages.
+  /// ```dart
+  /// ReactiveInputDecorator(
+  ///   formControlName: 'email',
+  ///   showErrors: (control) => control.invalid && control.touched && control.dirty,
+  /// ),
+  /// ```
+  ///
+  /// For documentation about the various parameters, see the [InputDecorator] class
+  /// and [InputDecorator], the constructor.
+  ReactiveInputDecorator(
+      {Key? key,
+      String? formControlName,
+      FormControl<dynamic>? formControl,
+      Map<String, ValidationMessageFunction>? validationMessages,
+      ControlValueAccessor<dynamic, dynamic>? valueAccessor,
+      ShowErrorsFunction<dynamic>? showErrors,
+
+      //////////////////////////////////////////////////////////////////////////
+      required Widget child,
+      InputDecoration? decoration})
+      : super(
+          key: key,
+          formControl: formControl,
+          formControlName: formControlName,
+          valueAccessor: valueAccessor,
+          validationMessages: validationMessages,
+          showErrors: showErrors,
+          builder: (field) {
+            final effectiveDecoration = (decoration ?? const InputDecoration())
+                .applyDefaults(Theme.of(field.context).inputDecorationTheme);
+
+            return IgnorePointer(
+              ignoring: !field.control.enabled,
+              child: Listener(
+                onPointerDown: (_) => field.control.markAsTouched(),
+                child: InputDecorator(
+                  decoration: effectiveDecoration.copyWith(
+                    errorText: field.errorText,
+                    enabled: field.control.enabled,
+                  ),
+                  child: child,
+                ),
+              ),
+            );
+          },
+        );
+}
