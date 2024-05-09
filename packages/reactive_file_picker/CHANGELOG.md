66f77e8911659d91a34a9d9687715d1104ed7c82
Vasiliy Ditsyak <vasilich6107@users.noreply.github.com>
2024-05-09 11:41:20 +0200

    reactive_file_picker 3.0.0

--- packages/reactive_file_picker/CHANGELOG.md
+++ packages/reactive_file_picker/CHANGELOG.md
@@ -1,3 +1,7 @@
+## [3.0.0]
+
+* file_picker package update
+
 ## [2.0.1]
 
 * Support `reactive_forms: 17.x`
--- packages/reactive_file_picker/lib/multi_file.freezed.dart
+++ packages/reactive_file_picker/lib/multi_file.freezed.dart
@@ -12,7 +12,7 @@ part of 'multi_file.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 /// @nodoc
 mixin _$MultiFile<T> {
@@ -134,7 +134,7 @@ class _$MultiFileImpl<T> extends _MultiFile<T> {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$MultiFileImpl<T> &&
