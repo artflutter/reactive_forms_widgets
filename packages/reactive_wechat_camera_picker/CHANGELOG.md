f8030f092616091e26fdf375ca658be6378844d4
Vasiliy Ditsyak <vasilich6107@users.noreply.github.com>
2024-06-25 15:29:55 +0200

    reactive_wechat_camera_picker 4.0.0

--- packages/reactive_wechat_camera_picker/CHANGELOG.md
+++ packages/reactive_wechat_camera_picker/CHANGELOG.md
@@ -1,3 +1,7 @@
+## [4.0.0]
+
+* wechat_camera_picker: ^4.3.0+1
+
 ## [2.0.1]
 
 * Support `reactive_forms: 17.x`
--- packages/reactive_wechat_camera_picker/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/reactive_wechat_camera_picker/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import photo_manager
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
