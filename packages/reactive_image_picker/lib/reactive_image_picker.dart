e8b89bcf4a3d0edcbe1f0a7053068a9d592269bf
Anas Almojahed <Anas.h.a.a.95@Gmail.com>
2024-12-18 23:50:37 +0300

    Update reactive_image_picker.dart
    reverted to old

--- packages/reactive_image_picker/lib/reactive_image_picker.dart
+++ packages/reactive_image_picker/lib/reactive_image_picker.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: use_build_context_synchronously
 
-library;
+// library;
 
-export 'src/reactive_image_picker.dart';
-export 'src/image_file.dart';
-export 'src/image_picker_widget.dart';
-export 'src/image_view.dart';
-export 'src/selected_file_view.dart';
-export 'src/video_view.dart';
-export 'src/widget_popup_dialog.dart';
+// export 'src/reactive_image_picker.dart';
+// export 'src/image_file.dart';
+// export 'src/image_picker_widget.dart';
+// export 'src/image_view.dart';
+// export 'src/selected_file_view.dart';
+// export 'src/video_view.dart';
+// export 'src/widget_popup_dialog.dart';
