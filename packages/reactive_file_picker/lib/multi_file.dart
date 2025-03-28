5d76c506cdcd3bfca2c8ea83ac8680b25db60be4
Francesco Berardi <francesco.berardi1988@gmail.com>
2025-03-28 14:51:23 +0100

    reactive_file_picker: Update freezed to version "^3.0.0"
    - Bump freezed version to "^3.0.0"
    - Use abstract class and regenerate the code

--- packages/reactive_file_picker/lib/multi_file.dart
+++ packages/reactive_file_picker/lib/multi_file.dart
@@ -4,9 +4,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'multi_file.freezed.dart';
 
 @freezed
-class MultiFile<T> with _$MultiFile<T> {
-  const MultiFile._();
-
+abstract class MultiFile<T> with _$MultiFile<T> {
   const factory MultiFile({
     @Default([]) List<T> files,
     @Default([]) List<PlatformFile> platformFiles,
