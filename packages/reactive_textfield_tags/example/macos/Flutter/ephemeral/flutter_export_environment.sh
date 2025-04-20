565c82a1303da9af291dc8e980b01880e5e8b43c
Vasiliy Ditsyak <vasilich6107@users.noreply.github.com>
2025-04-20 17:11:26 +0200

    dd

--- packages/reactive_textfield_tags/example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ packages/reactive_textfield_tags/example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/vasilich/fvm/versions/3.24.5"
+export "FLUTTER_ROOT=/Users/vasilich/fvm/versions/3.27.4"
 export "FLUTTER_APPLICATION_PATH=/Users/vasilich/IdeaProjects/reactive_forms_widgets/packages/reactive_textfield_tags/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
